46ca41f8f8369f101d97512857764ef166de44c9
wheaney <42350981+wheaney@users.noreply.github.com>
2024-01-04 13:18:29 -0800

    Add explicit checkout of v0.5.6 commit hash

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,6 +5,11 @@ pacman -S --noconfirm git jq
 
 git clone https://github.com/wheaney/decky-xrealAir
 
+# this branch represents an older version of the plugin on a deprecation path, force this to build on an older commit
+pushd decky-xrealAir
+git checkout tags/v0.5.6-beta
+popd
+
 # iterate over the custom_remote_binary list in package.json, download and check hashes, and finally copy if matching
 jq -r '.custom_remote_binary[] | "\(.name) \(.sha256hash) \(.url)"' decky-xrealAir/package.json | while read name hash url; do
     curl -Ls "$url" -o "/tmp/$name"
